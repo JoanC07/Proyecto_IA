4d89d87bd2023c6d6926fb1e2b661803b009bdb0
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-06-08 22:04:16 -0500

    Definimos nuestro objetivo y creamos el diccionario

---
+++ agente.py
@@ -0,0 +1,20 @@
+objetivo = {'Gallinas': '0',
+            'Perros': '0',
+            'Cerdos': '0',
+            'Burros': '0',
+            'Chivos': '0',
+            'Vacas': '0',
+            'Conejos': '0',
+            'Patos': '0',
+            'Ovejas': '0'}
+
+def Search(objetivo):
+    print('Elementos')
+    print(objetivo)
+    costo = 0
+    ubicacion_comedero = input("Ingrese El nombre del animal: ")
+    estado = input("Ingrese el estado del " + ubicacion_comedero + ": ")
+    Lista_objetos = []
+
+
+Search(objetivo)
