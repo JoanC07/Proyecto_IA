1dd751d6f9faca878e0651ce8b6f48e945a76233
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-06-10 02:36:55 -0500

    Documentación del codigo para pruebas unitarias del agente

--- prueba_agente.py
+++ prueba_agente.py
@@ -1,12 +1,12 @@
 
-'''Importamos la inittest
-Nos permite realizar pruebas unitarias sin mucho esfuerzo'''
+'''
+Importacion de librerias para prueba unitarias
+de archivo py correspondiente al agente
+Crea dicionarios para evaluar la funcion Buscar
+'''
 import  unittest 
-#Enlazamos con el .py a realizar las pruebas unitarias
-from BFS import graph_
-
-from agente import Search
-# dicionarios para evaluar la funcion buscar
+from agente import Buscar
+# 
 objetivo = {'Gallinas': '0',
  'Perros': '0', 
  'Cerdos': '0',
@@ -17,21 +17,16 @@ objetivo = {'Gallinas': '0',
  'Patos': '0',
  'Ovejas': '0' } 
 
-'''
-Comprueba una respuesta específica a un conjunto particular de entradas. 
-unittest proporciona una clase base.
-------------------------------------
-TestCase, que se puede usar para crear nuevos casos de prueba. Banco de pruebas.
-'''
-class check(unittest.TestCase):
-    
+class prueba(unittest.TestCase):
     '''
-    Comprobamos que nuestro agente cumpla la cantidad del objetivo con el costo final
-    '''
-    def test_agent(self):
-        g = Search(objetivo)
+    Clase prueba 
+    metodo prueba_agente
+        instanciamos la funcion que se va evaluar
+        con la funcion de unittest(parametros para evaluar)
+    '''   
+    def prueba_agente(self):
+        g = Buscar(objetivo)
         self.assertEqual(g ,18)
-
 if __name__ == "__main__":
     unittest.main()
     
