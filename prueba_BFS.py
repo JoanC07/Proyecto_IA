9d9fd22a5cefe3146781bda16197f14565b437ef
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-06-10 02:25:11 -0500

    Comentarios

--- prueba_BFS.py
+++ prueba_BFS.py
@@ -1,15 +1,24 @@
-
+'''
+Importamos la inittest
+Nos permite realizar pruebas unitarias sin mucho esfuerzo
+'''
 import  unittest 
-
+#Enlazamos con el .py a realizar las pruebas unitarias
 from BFS import grafo_
-
+'''
+Comprueba una respuesta específica a un conjunto particular de entradas. 
+unittest proporciona una clase base.
+------------------------------------
+TestCase, que se puede usar para crear nuevos casos de prueba. Banco de pruebas.
+'''
 class check(unittest.TestCase):
     
+    #Probamos el grafo ingresado manualmente del py BFS
     def prueba_grafo(self):
         grafo = grafo_(1, 6)
         
         self.assertEqual(grafo ,[1, 0, 2, 6])
-        
+    
 if __name__ == "__main__":
     unittest.main()
            
