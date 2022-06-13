44a71bd99c1d3fd56220fbcedcf5acfbc65db686
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-06-13 00:11:54 -0500

    Comentarios

--- prueba_BFS.py
+++ prueba_BFS.py
@@ -4,20 +4,24 @@ Nos permite realizar pruebas unitarias sin mucho esfuerzo
 '''
 import  unittest 
 #Enlazamos con el .py a realizar las pruebas unitarias
-from BFS import grafo_
+from BFS import grafo_ , verificar_nodo
 '''
 Comprueba una respuesta específica a un conjunto particular de entradas. 
 unittest proporciona una clase base.
 ------------------------------------
 TestCase, que se puede usar para crear nuevos casos de prueba. Banco de pruebas.
 '''
-class check(unittest.TestCase):
+class check(unittest.TestCase): #Clase Cheak
     
     #Probamos el grafo ingresado manualmente del py BFS
-    def prueba_grafo(self):
-        grafo = grafo_(1, 6)
-        
-        self.assertEqual(grafo ,[1, 0, 2, 9])
+    def test_grafo(self): #Sentencia para el test
+        grafo = grafo_() #Verificacion del grafo
+        self.assertEqual(grafo ,[1, 0, 2, 6])#Recorrido a esperar
+    
+    #Revisamos si los nodos estan dentro del rango 
+    def test_verficacion(self): #Sentencia para el test
+        verificado = verificar_nodo() #Verificacion del nodo
+        self.assertEqual(verificado,'Nodos dentro rango')#Mensaje dentro del rango
 
 if __name__ == "__main__":
     unittest.main()
