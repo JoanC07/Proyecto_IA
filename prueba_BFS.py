d4f50472fe12283ca0b4925ac017e4188a158b59
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-06-13 00:07:16 -0500

    Validacion, test 2

--- prueba_BFS.py
+++ prueba_BFS.py
@@ -17,8 +17,8 @@ class check(unittest.TestCase):
     def prueba_grafo(self):
         grafo = grafo_(1, 6)
         
-        self.assertEqual(grafo ,[1, 0, 2, 6])
-    
+        self.assertEqual(grafo ,[1, 0, 2, 9])
+
 if __name__ == "__main__":
     unittest.main()
            
