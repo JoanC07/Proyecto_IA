bce97f194396567b922205bff85857a54bf6aa6d
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-06-14 20:20:38 -0500

    Documentación de codigo para prueba BFS

--- prueba_BFS.py
+++ prueba_BFS.py
@@ -12,6 +12,18 @@ unittest proporciona una clase base.
 TestCase, que se puede usar para crear nuevos casos de prueba. Banco de pruebas.
 '''
 class check(unittest.TestCase): #Clase Cheak
+    '''Clase prueba 
+    Parametros:
+    unittest.TestCase
+    Procedimientos:
+    Procedimiento test_grafo
+        instanciamos la funcion que se va evaluar
+        con la funcion de unittest(parametros para evaluar)
+    Procedimiento test_verificacion
+        instanciamos la funcion que se va evaluar
+        Se le añaden parametro de evaluacion y verifica
+    Retorna: Ninguno
+    ''' 
     
     #Probamos el grafo ingresado manualmente del py BFS
     def test_grafo(self): #Sentencia para el test
